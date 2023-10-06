0427fce94afa3e8c39e575d55ff3dae3c0213b00
Philipp Hess <philipp-hess@hotmail.de>
2023-10-06 12:08:54 +0200

    Task1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // phew… not a lot going on here. Please add some code!
+
+const bookmark = document.querySelector('[data-js="bookmark"]');
+const answerText = document.querySelector('[data-js="text"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
+
+const hideShowButtonAnswerButton = document.querySelector(
+  '[data-js="right-answer"]'
+);
+hideShowButtonAnswerButton.addEventListener("click", () => {
+  //    Antworttext ein/ausblenden
+  answerText.classList.toggle("card__answer--active");
+
+  if (answerText.classList.contains("card__answer--active")) {
+    hideShowButtonAnswerButton.textContent = "hide answer";
+  } else {
+    hideShowButtonAnswerButton.textContent = "show answer";
+  }
+});
